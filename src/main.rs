cfa4a590c531f7a63a41d42035426e5eb61e60e9
Jeremy Carter <jeremy@jeremycarter.ca>
2020-08-04 11:37:56 -0400

    Add "real_main() -> i32" to support exit codes.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ use signal_gen_cjds66_lib::serial::open;
 use signal_gen_cjds66_lib::command::*;
 
 fn main() {
+    let exit_code = real_main();
+    std::process::exit(exit_code);
+}
+
+fn real_main() -> i32 {
     let mut args = env::args_os();
     
     if args.len() < 2 {
         println!("Error: The first argument must be the serial port to connect to, such as: /dev/ttyUSB0");
-        return;
+        return 1;
     }
 
     let mut port = &mut open(&args.nth(1).unwrap()).unwrap();
@@ -17,4 +22,6 @@ fn main() {
     get_machine_model(&mut port).unwrap();
 
     get_machine_number(&mut port).unwrap();
+
+    0
 }
